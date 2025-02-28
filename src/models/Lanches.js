21d5330c3b4094b5c6ebd54a6700b6499ec373f6
JEAN ANDRADE SOARES <jean.asoares@senacsp.edu.br>
2025-02-27 22:20:28 -0300

    Update files

--- src/models/Lanches.js
+++ src/models/Lanches.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose')
 
 const lancheSchema = new mongoose.Schema({
-    nomeLanche: { type: String, required: true, unique: true },
-    descricao: { type: String, required: true },
-    preco: { type: Number, required: true, min: 0 }
+    nomeLanche: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    descricao: { 
+        type: String, 
+        required: true 
+    },
+    preco: { 
+        type: Number, 
+        required: true, 
+        min: 0 
+    }
 }, {
     timestamps: true
 })
